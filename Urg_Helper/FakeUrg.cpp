91f7806f88bc9a4e102ebd7290ae4e1fa0dcdfd1
George Mitchell <gpv0001@uah.edu>
2015-02-03 12:12:16 -0600

    FakeUrg now is able to read from sim files. Ugh.

--- Urg_Helper/FakeUrg.cpp
+++ Urg_Helper/FakeUrg.cpp
@@ -1,14 +1,80 @@
 #include "FakeUrg.h"
+#include <limits>
+#include <algorithm>
+#include <iterator>
 
+namespace
+{
+	template <typename T> // T models Any
+	struct static_cast_func
+	{
+	  template <typename T1> // T1 models type statically convertible to T
+	  T operator()(const T1& x) const { return static_cast<T>(x); }
+	};
+}
 
-void FakeUrg::close() {}
-double FakeUrg::index2rad(int scanNo) { return 0.0; }
+// Todo: Close urg filehandle
+void FakeUrg::close() 
+{
+	_inFile.close();
+}
+
+// Todo: search array of angles for this index and then return corresponding angle value
+double FakeUrg::index2rad(int scanNo) const
+{
+	if (scanNo < _angleValues.size())
+	{
+		return _angleValues[scanNo];
+	}
+	else return std::numeric_limits<double>::quiet_NaN(); 
+}
+
+// open lidar file and parse out angle array
 bool FakeUrg::open(const char* device_name, 
 				   long baudrate,
-                   connection_type_t type) { return false; }
-void FakeUrg::start_measurement() {}
+                   connection_type_t type) 
+{
+	_inFile.open(device_name, std::fstream::in);
+
+	char endOfAngles[4] = {0, 0, 0, 0};
+	auto it = std::search(
+		std::istreambuf_iterator<char> (_inFile.rdbuf()), 
+		std::istreambuf_iterator<char> (), 
+		endOfAngles, 
+		endOfAngles + 4);
+
+	if (it != std::istreambuf_iterator<char> ())
+	{
+		int nElem = (_inFile.tellg())/sizeof(float);
+		_inFile.seekg(0, std::ios_base::beg);
+
+		_angleValues.resize(nElem);
+
+		std::transform(std::istreambuf_iterator<char> (_inFile.rdbuf()),
+			it,
+			_angleValues.begin(),
+			static_cast_func<float>());
+	}
+
+	return true;
+}
+
+// parse remainder of file
+bool FakeUrg::start_measurement(measurement_type_t,
+                               int scan_times,
+                               int skip_scan)
+{
+	return true;
+}
+
+// pop next distance measurement from list
 bool FakeUrg::get_distance(std::vector<long, std::allocator<long>> &data, long *timestamp) { return false; }
+
+// do nothing.
 bool FakeUrg::set_sensor_time_stamp(long time_stamp) { return false; }
 
+// meh
 FakeUrg::FakeUrg() {}
+
+// meh
 FakeUrg::~FakeUrg() {}
--- Urg_Helper/FakeUrg.h
+++ Urg_Helper/FakeUrg.h
@@ -1,22 +1,36 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <fstream>
 #include "Urg_driver.h"
+
+struct DataReading
+{
+	std::vector<long, std::allocator<long>> data;
+	long timestamp;
+};
 
 class FakeUrg : public qrk::Urg_driver
 {
 public:
-	MOCKABLE void close();
-	MOCKABLE double index2rad(int scanNo);
+	MOCKABLE void close() override;
+	MOCKABLE double index2rad(int scanNo) const override;
 	MOCKABLE bool open(const char* device_name, 
 				   long baudrate = Default_baudrate,
-                   connection_type_t type = Serial);
-	MOCKABLE void start_measurement();
-	MOCKABLE bool get_distance(std::vector<long, std::allocator<long>> &data, long *timestamp);
+                   connection_type_t type = Serial) override;
+	MOCKABLE bool start_measurement(measurement_type_t type = Distance,
+                               int scan_times = Infinity_times,
+                               int skip_scan = 0) override;
+	MOCKABLE bool get_distance(std::vector<long, std::allocator<long>> &data, long *timestamp) override;
 
-	MOCKABLE bool set_sensor_time_stamp(long time_stamp);
+	MOCKABLE bool set_sensor_time_stamp(long time_stamp) override;
 
 	FakeUrg();
 	~FakeUrg();
+
+private:
+	std::vector<DataReading> _rangeData;
+	std::vector<float> _angleValues;
+	std::fstream _inFile;
 };
 
