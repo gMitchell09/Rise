a949d42ac5804939eb12e548b6381ff8028e31a6
George Mitchell <gpv0001@uah.edu>
2015-03-15 09:42:24 -0500

    Working on pathfinding grid
    Now that I can partition the point cloud into partitions I can process
    each "column" of points and classify it as a "Wall", "Stairs", "Open",
    "Unknown"... just need to develop a classifier.

--- Urg_Helper/TravelGrid.cpp
+++ Urg_Helper/TravelGrid.cpp
@@ -1,14 +1,43 @@
 #include "TravelGrid.h"
 
-#include <boost/interprocess/mapped_region.hpp>
+const size_t TravelGrid::cell_size = 200; // 200mm
 
 TravelGrid::TravelGrid()
 {
-	_pageSize = boost::interprocess::mapped_region::get_page_size();
-	quadTree = vtkHyperOctree::New();
+
 }
 
+TravelGrid::Cell& TravelGrid::cell_at(float x, float y)
+{
+	CellPoint p;
+	p.x = (int)(x / 200);
+	p.y = (int)(y / 200);
+
+	auto search = grid.find(p);
+	bool exists = (search != grid.end());
+
+	Cell& cell = grid[p];
+	if (!exists) cell.cellType = Cell::kUndefined;
+
+	return cell;
+}
 
 TravelGrid::~TravelGrid()
 {
+}
+
+TravelGrid::Cell::CellTypes TravelGrid::Cell::classify_cell(pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud)
+{
+	float floorHeightGuess = 0.0;
+	float ceilingHeightGuess = 3.0;
+	float mean = 0, median = 0, mode = 0, min = 9999999, max = -99999999;
+
+	for (auto itr = cloud->begin(); itr != cloud->end(); ++itr)
+	{
+		(*itr).x;
+		(*itr).y;
+		(*itr).z;
+	}
+
+	return Cell::kUndefined;
 }
--- Urg_Helper/TravelGrid.h
+++ Urg_Helper/TravelGrid.h
@@ -1,38 +1,63 @@
 #pragma once
-#include <vtkHyperOctree.h>
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/passthrough.h>
+
+#include <memory> // for std:: smart pointers
+#include <map>
 
 class TravelGrid
 {
 public:
-	enum CellTypes {
-		kUndefined,
-		kFloor,
-		kWall,
-		kStairs,
-	};
-
 	struct Cell {
+		enum CellTypes {
+			kUndefined,
+			kFloor,
+			kWall,
+			kStairs,
+		};
+
 		CellTypes cellType;
+
+		CellTypes classify_cell(pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud);
 	};
 
-	struct CellEntry {
-		Cell* cell;
-		TravelGrid* xGrid;
-		TravelGrid* yGrid;
+	struct CellPoint {
+		int x, y;
+
+		bool operator() (const CellPoint& lhs, const CellPoint& rhs) const
+		{
+			return (lhs.x*lhs.x + lhs.y*lhs.y) < (rhs.x * rhs.x + rhs.y * rhs.y);
+		}
 	};
+
+	static const size_t cell_size;
 
 	TravelGrid();
 	~TravelGrid();
 
-	vtkHyperOctree *quadTree;
+	Cell& cell_at(float x, float y);
 
 private:
-	size_t _pageSize;
-	size_t _width;
-	size_t _height;
-
-	long _posX;
-	long _posY;
-
-	CellEntry** _cells;
+	std::map<CellPoint, Cell, CellPoint> grid;
 };
+
+pcl::PointCloud<pcl::PointXYZ>::ConstPtr points_in_range(pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud, float x_start, float x_end, float y_start, float y_end)
+{
+	pcl::PointCloud<pcl::PointXYZ>::Ptr filteredCloud(new pcl::PointCloud<pcl::PointXYZ>);
+	pcl::PointCloud<pcl::PointXYZ>::Ptr filteredCloud2(new pcl::PointCloud<pcl::PointXYZ>);
+
+	pcl::PassThrough<pcl::PointXYZ> filter;
+	filter.setInputCloud(cloud);
+	filter.setFilterFieldName("x");
+	filter.setFilterLimits(x_start, x_end);
+	filter.filter(*filteredCloud);
+
+	filter.setInputCloud(filteredCloud);
+	filter.setFilterFieldName("y");
+	filter.setFilterLimits(y_start, y_end);
+	filter.filter(*filteredCloud2);
+
+	return filteredCloud2->makeShared();
+}
